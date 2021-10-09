256bbfc4c274445cc0b084f30085bc8df28641b9
Phạm Ngọc <you@example.com>
2021-10-09 17:40:19 +0700

    change requirements

--- main.py
+++ main.py
@@ -106,5 +106,5 @@ app=create_app()
 # first load the model and then start the server
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=5000, debug=True)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True, threaded=True, use_reloader=False)
